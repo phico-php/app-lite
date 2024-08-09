cb1c68d21f7fd26281ff694e7d73979ee633957a
indgy <65541311+indgy@users.noreply.github.com>
2024-08-09 09:16:45 +0100

    Removed unnecessary config

--- config/container.php
+++ config/container.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+
+    'path' => env('CONTAINER_PATH', 'app/container.php'),
     'autowire' => env('CONTAINER_AUTOWIRE', true),
     'share' => env('CONTAINER_SHARE', false),
+
 ];
